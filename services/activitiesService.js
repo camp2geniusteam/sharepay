ec096f1be9baeb716f91f176d8296630878b6da7
cdedkt <christophe.delattre@decathlon.com>
2018-05-14 16:46:04 +0200

    get activities by user

--- services/activitiesService.js
+++ services/activitiesService.js
@@ -2,8 +2,7 @@ const activities = require("../entities/activities");
 
 
 function getActivityWithDetail(activityId) {
-  return activities.findById(activityId);
-  /*
+  return activities.findById(activityId);/*
   .then(activity => Promise.all([categories.getCategoryFromProduct(product.id), brands.findById(product.brand_id)])
 	.then(values => {
 	  product.categories = values[0];
@@ -15,11 +14,32 @@ function getActivityWithDetail(activityId) {
 }
 
 function getAllActivities() {
-  return activities.findAll();
+  return activities.findAll()
+  .then(activities => {
+    return activities.map(activity => {
+      activity.total_amount = 200;
+      activity.members = [{firstname: "christophe", lastname: "Delattre"},
+        {firstname: "damien", lastname: "Desprez"}];
+        return activity;
+      });
+  });
+}
+
+function getAllActivitiesByUser(email, activityStatus) {
+  return activities.findAll()
+  .then(activities => {
+    return activities.map(activity => {
+      activity.total_amount = 200;
+      activity.members = [{firstname: "christophe", lastname: "Delattre"},
+        {firstname: "damien", lastname: "Desprez"}];
+        return activity;
+      });
+  });
 }
 
 
 module.exports = {
   getActivityWithDetail: getActivityWithDetail,
-  getAllActivities: getAllActivities
+  getAllActivities: getAllActivities,
+  getAllActivitiesByUser: getAllActivitiesByUser
 }
